07c41961a5053f9188b992642ab2020dc71c9fad
Arsha Niksa <arshanyxaa@gmail.com>
2026-07-05 15:52:16 +0330

    toggle test

--- docs/js/for-l-toggle.js
+++ docs/js/for-l-toggle.js
@@ -1,6 +1,6 @@
 /* Toggle the private anniversary link in the menu.
    Set SHOW_FOR_L to true before the anniversary, or false to hide it. */
-const SHOW_FOR_L = false;
+const SHOW_FOR_L = true;
 
 function applyForLToggle() {
     document.querySelectorAll('[data-feature="for-l"]').forEach((forLMenuItem) => {
